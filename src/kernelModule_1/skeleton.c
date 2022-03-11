8047aca34f3cb333ab89b848262a479ca3c810a2
gaetankolly <gaetan.kolly@hes-so.ch>
2022-03-11 16:13:34 +0000

    kenelModule_1

---
+++ src/kernelModule_1/skeleton.c
@@ -0,0 +1,21 @@
+// skeleton.c
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+// needed by all modules
+// needed for macros
+// needed for debugging
+static int __init skeleton_init(void)
+{
+pr_info ("Linux module skeleton loaded\n");
+return 0;
+}
+static void __exit skeleton_exit(void)
+{
+pr_info ("Linux module skeleton unloaded\n");
+}
+module_init (skeleton_init);
+module_exit (skeleton_exit);
+MODULE_AUTHOR ("Daniel Gachet <daniel.gachet@hefr.ch>");
+MODULE_DESCRIPTION ("Module skeleton");
+MODULE_LICENSE ("GPL");
