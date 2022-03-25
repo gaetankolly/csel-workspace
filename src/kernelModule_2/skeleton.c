6d524580d73afff01e3020977deb84d84441846a
gaetankolly <gaetan.kolly@hes-so.ch>
2022-03-25 13:21:29 +0000

    Exercice 4 allocation, deallocation to be added

---
+++ src/kernelModule_2/skeleton.c
@@ -0,0 +1,65 @@
+// skeleton.c
+#include <linux/module.h>	// needed by all modules
+#include <linux/init.h>		// needed for macros
+#include <linux/kernel.h>	// needed for debugging
+
+#include <linux/moduleparam.h>	// needed for module parameters
+
+/* function needed for list */
+#include <linux/slab.h>
+#include <linux/list.h>		
+#include <linux/string.h>	
+
+
+// parameters
+static char* elementText = "This is a list of element"; // First element
+module_param(elementText, charp, 0664); 	
+static int  nbElements = 10;			// nb of element which will be created  
+module_param(nbElements, int, 0);
+
+
+// list
+#define ELEMENT_STRING_SIZE
+struct element {
+	char text[ELEMENT_STRING_SIZE];
+	int element_id;
+	struct list_head node;
+};
+
+static LIST_HEAD (list); // head of the list, global variable
+
+// allocate on element and add it at the tail of the list
+void alloc_element (int id) {
+	struct element* newElement
+	newElement = kzalloc(sizeof(*newElement), GFP_KERNEL); // create a new element on heap
+	if (newElement != NULL){
+		newElement->element_id=id;
+		strncpy(newElement->text, elementText, ELEMENT_STRING_SIZE-1);
+		list_add_tail(&ele->node, &list); // add element at the end of the list
+	}
+}
+
+
+static int __init skeleton_init(void)
+{
+	pr_info ("Linux module 01 skeleton loaded\n");
+	pr_debug ("  text: %s\n  elements: %d\n", elementText, nbElements);
+
+	for (i = 0; i < nbElements; i++) {
+		alloc_element(i);
+	}
+
+	return 0;
+}
+
+static void __exit skeleton_exit(void)
+{
+	pr_info ("Linux module skeleton unloaded\n");
+}
+
+module_init (skeleton_init);
+module_exit (skeleton_exit);
+
+MODULE_AUTHOR ("Andrea Enrile; Gaëtan Kolly");
+MODULE_DESCRIPTION ("Module skeleton");
+MODULE_LICENSE ("GPL");
