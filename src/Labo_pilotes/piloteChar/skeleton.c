87385f586dfc56afb8cff2d5afed7468849603be
gaetankolly <gaetan.kolly@hes-so.ch>
2022-04-07 19:37:51 +0000

    driver_Exo2

--- src/Labo_pilotes/piloteChar/skeleton.c
+++ src/Labo_pilotes/piloteChar/skeleton.c
@@ -6,10 +6,17 @@
 #include <linux/module.h>      /* needed by all modules */
 #include <linux/moduleparam.h> /* needed for module parameters */
 #include <linux/uaccess.h>     /* needed to copy data to/from user */
+#include <linux/slab.h>        /* needed for dynamic memory management */
 
 #define BUFFER_SZ 10000
+#define NB_INSTANCE_MAX 5
 
-static char buffer[BUFFER_SZ];      // memory representing the file
+static int instances = 1;
+module_param(instances, int, 0);
+
+//static char buffer[BUFFER_SZ];      // memory representing the files
+static char** buffers;              // memory representing the files
+
 static dev_t skeleton_dev;          // contain minor and major number, register with alloc_chrdev_region
 static struct cdev skeleton_cdev;   // representation of the driver into the kernel, 
 
@@ -20,7 +27,8 @@ static ssize_t skeleton_read(struct file* f,  		// only used to read file attrib
 {
 
     ssize_t remaining = BUFFER_SZ - (ssize_t)(*off); // remaining size
-    char* ptr         = buffer + *off;               // position where to read
+    //char* ptr = buffer + *off;                    // at the position required
+    char* ptr = (char*) f->private_data + *off;       // at the position required
     if (count > remaining) count = remaining;        // security against buffer over read
     *off += count;                                   // update offset for the user
     // copy required number of bytes
@@ -51,7 +59,8 @@ static ssize_t skeleton_write(struct file* f,           // only used to read fil
     }
     else{
 		int i;
-        char* ptr = buffer + *off; // at the position required
+        //char* ptr = buffer + *off; // at the position required
+        char* ptr = (char*) f->private_data + *off; // at the position required
         *off += count;             // update offset in case of append for the user
         ptr[count] = 0;  // make sure string is null terminated
         if (copy_from_user(ptr, buf, count)) count = -EFAULT;
@@ -83,6 +92,10 @@ static int skeleton_open(struct inode* i, struct file* f)
     } else if ((f->f_mode & FMODE_WRITE) != 0) {
         pr_info("skeleton : opened for writing...\n");
     }
+
+    f->private_data = buffers[iminor(i)];
+    pr_info("skeleton: private_data=%p\n", f->private_data);
+
     return 0;
 }
 
@@ -103,17 +116,30 @@ static struct file_operations skeleton_fops = {
 };
 
 static int __init skeleton_init(void)
-{
+{   
+    int i;
+    int status; 
 	// skeleton_dev = de type dev_t containt the major and minor number (32bits) which represent the device driver
 	// skeleton_cdev = struct cdev 
 	// alloc_chrdev_region allocate dymicaly the minor and major number in skeleton_dev
 	//int alloc_chrdev_region (dev_t* dev, unsinged baseminor, unsinged count, const char* name);
-    int status = alloc_chrdev_region(&skeleton_dev, 0, 1, "mymodule");
+    if (instances > NB_INSTANCE_MAX){
+        instances=NB_INSTANCE_MAX;
+        pr_warn("Maximal number of instance is %d, instances set to %d\n",NB_INSTANCE_MAX,NB_INSTANCE_MAX);
+    }
+    status = alloc_chrdev_region(&skeleton_dev, 0, instances, "mymodule");
     if (status == 0) {
 		// init cdev structure, make it ready to add to the system with cdev_add
         cdev_init(&skeleton_cdev, &skeleton_fops);
         skeleton_cdev.owner = THIS_MODULE;
-        status              = cdev_add(&skeleton_cdev, skeleton_dev, 1); // add character device to the system
+        status = cdev_add(&skeleton_cdev, skeleton_dev, instances); // add character device to the system
+    }
+
+    if (status == 0) {
+        buffers = kzalloc(sizeof(char*) * instances, GFP_KERNEL);
+        for (i = 0; i < instances; i++){
+            buffers[i] = kzalloc(BUFFER_SZ, GFP_KERNEL);
+        }
     }
 
     pr_info("Linux module skeleton loaded\n");
@@ -122,8 +148,14 @@ static int __init skeleton_init(void)
 
 static void __exit skeleton_exit(void)
 {
+    int i;
     cdev_del(&skeleton_cdev);
-    unregister_chrdev_region(skeleton_dev, 1);
+    unregister_chrdev_region(skeleton_dev, instances);
+
+    for (i = 0; i < instances; i++){
+        kfree(buffers[i]);
+    }
+    kfree(buffers);
 
     pr_info("Linux module skeleton unloaded\n");
 }
